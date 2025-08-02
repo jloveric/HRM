5d51ca7d65ac4794b54d90e449f600101ad490b7
jloveric <john.loverich@gmail.com>
2025-08-02 14:23:46 -0700

    Use poetry

--- pretrain.py
+++ pretrain.py
@@ -16,7 +16,9 @@ import coolname
 import hydra
 import pydantic
 from omegaconf import DictConfig
-from adam_atan2 import AdamATan2
+#from adam_atan2 import AdamATan2
+from adam_atan2_pytorch import AdamAtan2
+
 
 from puzzle_dataset import PuzzleDataset, PuzzleDatasetConfig, PuzzleDatasetMetadata
 from utils.functions import load_model_class, get_model_source_path
